371d753eb47377c3cb789da157dc126c8bf89f28
khai956 <102295632+khai956@users.noreply.github.com>
2024-06-10 20:01:00 +0800

    First dp

---
+++ public/script.js
@@ -0,0 +1,58 @@
+(function () {
+    
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        // ...
+        apiKey: "AIzaSyCekhUNgktNe_beFMCld8y8_hyhRXvG7U0",
+            authDomain: "exattend.firebaseapp.com",
+            projectId: "exattend",
+            storageBucket: "exattend.appspot.com",
+            messagingSenderId: "555960313645",
+            appId: "1:555960313645:web:b1ab1d9f48051499ecd5a4",
+            measurementId: "G-TG55FBLZ97"
+      };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    //Get Elements
+    const txtEmail = document.getElementById("txtEmail");
+    const txtPassword = document.getElementById("txtPassword");
+    const btnLogin = document.getElementById("btnLogin");
+    const btnSignup = document.getElementById("btnSignup");
+
+    //Add Login Event
+    btnLogin.addEventListener('click', e => {
+        const email = txtEmail.value;
+        const password = txtPassword.value;
+
+        const auth = firebase.auth();
+
+        //sign in with firebase auth
+        auth.signInWithEmailAndPassword(email, password).then(user =>{
+            alert("Login Successful :)").defaultSuccessUrl("/homepage.html", true);
+        }).catch(err => {
+            alert(err.message);
+        });
+        
+    });
+
+    //Add Signup Event
+    btnSignup.addEventListener('click', e => {
+
+        //get email and password
+        const email = txtEmail.value;
+        const password = txtPassword.value;
+
+        const auth = firebase.auth();
+
+        //sign in with firebase auth
+        const promise = auth.createUserWithEmailAndPassword(email, password).then(user => {
+            alert("Signup Successful :)")
+        }).catch(err => {
+            alert(err.message);
+        });
+
+    });
+
+
+}());
